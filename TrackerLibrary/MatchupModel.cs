7142ab2932af66e183fd13f4d96860b59e43dfa7
D. Azevedo <rplazevedo@gmail.com>
2023-11-04 15:41:54 +0000

    Set up class structure. Finished Lesson 6

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchuppEntryModel> Entries { get; set; } = new List<MatchuppEntryModel> ();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
